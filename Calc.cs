60a0fcdc70fbd43380a76c2dcd61ac70ed373244
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-16 10:12:09 +0530

    Final Commit

---
+++ Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalApp
+{
+    public class Calc
+    {
+        public void Add(double n1, double n2) {
+            Console.WriteLine("Result After Addition: \t"+(n1+n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result After Sub: \t" + (n1 - n2));
+        }
+        public void Mul(double n1, double n2)
+        {
+            Console.WriteLine("Result After Mul: \t" + (n1 * n2));
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result After Div: \t" + (n1 / n2));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalApp;
+
+Console.WriteLine("Calculator App");
+Calc calc = new Calc();
+Console.WriteLine("Addition");
+calc.Add(12, 45);
+Console.WriteLine("Subraction");
+calc.Sub(12, 45);
+Console.WriteLine("Multiplication");
+calc.Mul(12, 45);
+Console.WriteLine("Division");
+calc.Div(12, 45);
